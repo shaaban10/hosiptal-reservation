913752969c20512de163d4bc35b8ca0cd3eeb8d2
shaaban alahmed <shaabanalahmed20@gmail.com>
2023-09-21 21:00:42 +0200

    add html and css ➕➕➕

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hospital reservation</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <div class="main">
+        <div class="logo">
+            <img src="./img/R.jpg" alt="">
+            <h2>Angel Hospital </h2>
+        </div>
+    </div>
+    <div class="book">
+
+    <p>Welcome to Angel Hospital, Please fill the inputs with your personal informations </p>
+    <form action="" method="">
+        <input type="text" placeholder="Enter Your Name" name="name" >
+        <input type="text" placeholder="Enter Your Email" name="email" >
+        <input type="date"  name="date" >
+        <input type="submit" value="reserve now"  name="submit" >
+
+    </form>
+    </div>
+
+</body>
+</html>
